31ace219e6531f01fea3961c9ce5f5da1a8ab507
lucas <lucasfaura@gmail.com>
2025-10-07 18:09:43 +0200

    nav fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -42,7 +41,6 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
